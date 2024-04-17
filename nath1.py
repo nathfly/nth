48291a799847a83247161d9c88dc41ddfe0908e1
nathfly <167087093+nathfly@users.noreply.github.com>
2024-04-17 12:03:08 +0200

    Add files via upload

---
+++ nath1.py
@@ -0,0 +1,24 @@
+"""
+a=12
+if  a > 10:
+    print("premier oui")
+a=5
+if a>10 :
+     print(", deuxième oui")
+
+
+input("mets un nombre ")
+if 2> 8 :
+    print("cou")
+else:
+    print("pou")
+print("dre")
+"""
+x=5
+if x<2:
+    print("petit")
+elif x<8:
+    print("Moyen")
+else:
+    print("grand")
+    
