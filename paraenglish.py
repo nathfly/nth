49fea833852753eaf1ea6cab2a5af90ce20a6864
nathfly <167087093+nathfly@users.noreply.github.com>
2024-04-19 14:48:18 +0200

    Add files via upload

--- paraenglish.py
+++ paraenglish.py
@@ -1,4 +1,5 @@
-a=input("Combien d'heures se sont ecoulées? ")
+
+"""a=input("Combien d'heures se sont ecoulées? ")
 b=input("Combien de minutes se sont écoulées? ")
 c=input("Combien de secondes se sont écoulées? ")
 a=int(a)
@@ -8,3 +9,17 @@ a1=(a*3600)
 b1=(b*60)
 c11=(c*1)
 print((c11)+(b1)+(a1))
+"""
+
+
+F=input("Combien de secondes se sont ecoulees? ")
+
+def seconde(F):
+heure = F // 3600
+    minute = (F - (3600*heure))//60
+    secondes = F-(3600*heure)-(60*minute)
+    time = (heures, minutes, secondes)
+    return time
+
+resultat = (seconde(456))
+print("% heures, % minutes et % secondes\n" %(resultat[0], resultat[1], resultat[2]))
